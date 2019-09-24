555b9be1563b9787651250fa88f883a42b299f02
Meuss <thomas.miller147@gmail.com>
2019-09-24 23:31:54 +0200

    changing maze

--- src/script.js
+++ src/script.js
@@ -1,19 +1,20 @@
 window.onload = setTimeout(load_animation(), 2000);
 
 function load_animation() {
-  single_animations('#solution', 5);
+  single_animations('#solution', 4);
 }
-function single_animations(id, x) {
-  var shape = document.querySelector(id);
+function single_animations(id, duration) {
+  const shape = document.querySelector(id);
   shape.classList.add('hidden');
   window.setTimeout(function() {
     shape.classList.remove('hidden');
-    var length = shape.getTotalLength();
+    const length = shape.getTotalLength();
     shape.style.transition = shape.style.WebkitTransition = 'none';
     shape.style.strokeDasharray = length + ' ' + length;
+    // shape.style.strokeDasharray = length / 100 + ' ' + length / 100 + ' ' + length / 100 + ' ' + length / 100;
     shape.style.strokeDashoffset = length;
     shape.getBoundingClientRect();
-    shape.style.transition = shape.style.WebkitTransition = 'stroke-dashoffset ' + x + 's linear';
+    shape.style.transition = shape.style.WebkitTransition = 'stroke-dashoffset ' + duration + 's linear';
     shape.style.strokeDashoffset = '0';
   }, 300);
 }
