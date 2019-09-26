de0b7181c89ac9d7e4e88b7c516958f5b289edc0
Meuss <thomas.miller147@gmail.com>
2019-09-26 23:38:35 +0200

    dotted animation running!

--- src/script.js
+++ src/script.js
@@ -1,24 +1,12 @@
-window.onload = setTimeout(load_animation(), 2000);
-
-function load_animation() {
-  single_animations('#solution', 4);
-}
-function single_animations(id, duration) {
-  const shape = document.querySelector(id);
-  shape.classList.add('hidden');
-  window.setTimeout(function() {
-    shape.classList.remove('hidden');
-    const length = shape.getTotalLength();
-    shape.style.transition = shape.style.WebkitTransition = 'none';
-    shape.style.strokeDasharray = length + ' ' + length;
-    // shape.style.strokeDasharray = length / 100 + ' ' + length / 100 + ' ' + length / 100 + ' ' + length / 100;
-    shape.style.strokeDashoffset = length;
-    shape.getBoundingClientRect();
-    shape.style.transition = shape.style.WebkitTransition = 'stroke-dashoffset ' + duration + 's linear';
-    shape.style.strokeDashoffset = '0';
-  }, 300);
-}
+window.onload = setTimeout(greensockanim(), 4000);
 
 document.querySelector('button').addEventListener('click', function() {
-  load_animation();
+  greensockanim();
 });
+
+function greensockanim() {
+  var paths = Array.prototype.slice.call(document.querySelectorAll('#solution path')).reverse();
+  var tl = new TimelineLite();
+  tl.set(paths, { opacity: 0 });
+  tl.staggerTo(paths, 0.1, { opacity: 1 }, 0.01, 0.1);
+}
